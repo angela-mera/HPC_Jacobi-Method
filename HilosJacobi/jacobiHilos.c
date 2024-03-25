1c0c43245a3838582e57fdae1c445effdbd9f18e
Angela María <117762749+angela-mera@users.noreply.github.com>
2024-03-25 13:46:29 -0500

    Added comments for improved code readability (jacobiHilos.c)

--- HilosJacobi/jacobiHilos.c
+++ HilosJacobi/jacobiHilos.c
@@ -4,15 +4,16 @@
 #include <string.h>
 #include <sys/time.h>
 
+// Global variables
 double *u, *f, *utmp;
 
-struct thread_info {
+struct thread_info { // Struct to hold the start and end indices of each thread
   int from;
   int to;
   double h2;
 };
 
-
+// Thread subroutine for the Jacobi method
 void *thread_subroutine(void *threadarg) {
   struct thread_info *thread_info;
   int from, to, i;
@@ -23,7 +24,7 @@ void *thread_subroutine(void *threadarg) {
   from = thread_info->from;
   to = thread_info->to;
 
-  
+  // Perform the calculations using the given indices
   for (i = from; i < to; ++i)
     utmp[i] = (u[i - 1] + u[i + 1] + h2 * f[i]) / 2;
 
@@ -31,38 +32,48 @@ void *thread_subroutine(void *threadarg) {
   for (i = from; i < to; ++i)
     u[i] = (utmp[i - 1] + utmp[i + 1] + h2 * f[i]) / 2;
 
+  // Exit the thread
   pthread_exit(NULL);
 }
 
+// Main function for the Jacobi method
 void jacobi(int nsweeps, int size, int threads_ammount) {
+  // Calculate the step size and init the required data structures
   int i, sweep, thread_idx;
   double h = 1.0 / size;
   double h2 = h * h;
 
   struct thread_info threads_info[threads_ammount];
   pthread_t threads[threads_ammount];
-
+  
+  // Initialize the first and last values of the array
   utmp[0] = u[0];
   utmp[size] = u[size];
 
+  // Perform the iterations
   for (sweep = 0; sweep < nsweeps; sweep += 2) {
+    // Create the required number of threads and initialize their data
     for (thread_idx = 0; thread_idx < threads_ammount; thread_idx++) {
 
       threads_info[thread_idx].h2 = h2;
       threads_info[thread_idx].from = thread_idx * (size - 1) / threads_ammount;
       threads_info[thread_idx].to =
           (thread_idx + 1) * (size - 1) / threads_ammount;
-
+      
+      // Set the boundaries of the first and last threads
       if (thread_idx == 0) {
         threads_info[thread_idx].from = 1;
       }
+      
+      // Create the threads
       if (pthread_create(&threads[thread_idx], NULL, thread_subroutine,
                          (void *)&threads_info[thread_idx])) {
         printf("There was an error creating a thread\n");
         exit(-1);
       }
     }
-
+    
+     // Join the threads and collect the results
     for (thread_idx = 0; thread_idx < threads_ammount; thread_idx++) {
       if (pthread_join(threads[thread_idx], NULL)) {
         printf("There was an error joining a thread\n");
@@ -72,6 +83,7 @@ void jacobi(int nsweeps, int size, int threads_ammount) {
   }
 }
 
+// Function to write the solution to a file
 void write_solution(int n, double *u, const char *fname) {
   int i;
   double h = 1.0 / n;
@@ -87,18 +99,19 @@ int main(int argc, char **argv) {
   char *fname;
   struct timeval tstart, tend;
 
-  
+  // Checking the input parameters and allocating the memory
   n = (argc > 1) ? atoi(argv[1]) : 100;
   nsteps = (argc > 2) ? atoi(argv[2]) : 100;
   threads_ammount = (argc > 3) ? atoi(argv[3]) : 4;
   fname = (argc > 4) ? argv[4] : NULL;
   h = 1.0 / n;
 
- 
+  // Allocate the required memory for u, f, and utmp
   u = (double *)malloc((n + 1) * sizeof(double));
   f = (double *)malloc((n + 1) * sizeof(double));
   utmp = (double *)malloc((n + 1) * sizeof(double));
 
+  // Initializing the u array
   memset(u, 0, (n + 1) * sizeof(double));
 
   for (i = 0; i <= n; ++i)
@@ -117,8 +130,10 @@ int main(int argc, char **argv) {
   if (fname)
     write_solution(n, u, fname);
 
+  // Free the allocated memory and return
   free(f);
   free(u);
   free(utmp);
+  
   return 0;
 }
